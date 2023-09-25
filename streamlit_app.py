79ceadf3efb2d3343b4bfa2401d7302cf8b94196
gabark44 <145971379+gabark44@users.noreply.github.com>
2023-09-25 10:39:31 +0200

    Update streamlit_app.py
    changes to the menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
-streamlit.header('Breakfast Menu')
+streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Oatmeal with peanut butter')
 streamlit.text('🥑🍞 Avocado toast')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
