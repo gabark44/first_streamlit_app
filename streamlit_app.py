36d3a17e7356b68912c7cb2445cc9e229a9c7445
gabark44 <145971379+gabark44@users.noreply.github.com>
2023-09-26 09:05:31 +0200

    Update streamlit_app.py
    if else

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,13 +21,15 @@ streamlit.dataframe(fruits_to_show)
 
 #display fruityvice api response
 streamlit.header('Fruityvice Fruit Advice')
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    streamlit.error("Please select a fruit to get information")
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalized)
 streamlit.write('The user entered ', fruit_choice)
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-#streamlit.text(fruityvice_response.json())
-#show data in a table
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-streamlit.dataframe(fruityvice_normalized)
 
 streamlit.stop()
 
